0b1d573c22b395be9106204e30a58a049b49b2e0
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-19 17:55:09 +0530

    04 - select for retrieval or to read data

---
+++ MySQL/04_select.sql
@@ -0,0 +1,28 @@
+USE college;
+
+SELECT
+  *
+FROM
+  students;
+
+SELECT
+  name
+FROM
+  students;
+
+SELECT
+  id,
+  age
+FROM
+  students;
+
+SELECT
+  name AS Student_Name
+FROM
+  students;
+
+SELECT
+  NOW();
+
+SELECT
+  100 + 500;
