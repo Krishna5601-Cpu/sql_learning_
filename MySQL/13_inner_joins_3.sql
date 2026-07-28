eeb1a4ac61d89954fc3b0e2fe41db6ef18bfde8b
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-28 23:15:05 +0530

    12 - more on inner joins_3

---
+++ MySQL/13_inner_joins_3.sql
@@ -0,0 +1,52 @@
+USE college;
+
+SELECT
+  s.first_name,
+  c.course_name
+FROM
+  enrollments e
+  JOIN students s ON e.student_id = s.id
+  JOIN courses c ON e.course_id = c.course_id
+WHERE
+  s.first_name = 'Krishna';
+
+SELECT
+  s.first_name,
+  c.course_name
+FROM
+  enrollments e
+  JOIN students s ON e.student_id = s.id
+  JOIN courses c ON e.course_id = c.course_id;
+
+SELECT
+  s.first_name,
+  c.course_name,
+  t.teacher_name
+FROM
+  enrollments e
+  JOIN students s ON e.student_id = s.id
+  JOIN courses c ON e.course_id = c.course_id
+  JOIN teachers t ON c.teacher_id = t.teacher_id;
+
+SELECT
+  s.first_name,
+  c.course_name,
+  t.teacher_name,
+  d.department_name
+FROM
+  enrollments e
+  JOIN students s ON e.student_id = s.id
+  JOIN courses c ON e.course_id = c.course_id
+  JOIN teachers t ON c.teacher_id = t.teacher_id
+  JOIN departments d ON c.department_id = d.department_id;
+
+SELECT
+  c.course_name,
+  COUNT(*) AS total_students
+FROM
+  enrollments e
+  JOIN courses c ON e.course_id = c.course_id
+GROUP BY
+  c.course_name;
+
+  
